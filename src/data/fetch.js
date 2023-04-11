22e79fbf21d7343a1c927e6b65b60a08c83445c9
0xTheKnightmare <pdiuec@gmail.com>
2023-04-11 13:38:49 +0530

    added randomized useragents

--- src/data/fetch.js
+++ src/data/fetch.js
@@ -5,6 +5,22 @@ const priceFetch = async () => {
   const priceData = {};
 
   const fetchPrice = (symbol) => {
+    const userAgents = [
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.4515.131 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.4515.131 Safari/537.36'
+    ];
+  
+    const randomUserAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
+    const options = {
+      headers: {
+        'User-Agent': randomUserAgent,
+      },
+    };
+
     return fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${symbol}USDT`)
       .then(response => response.json())
       .then(data => {
@@ -34,7 +50,7 @@ const PriceTable = () => {
 
     const intervalId = setInterval(() => {
       fetchData();
-    }, 1000);
+    },2000);
 
     return () => clearInterval(intervalId);
   }, []);
--- src/data/test .js
+++ src/data/test .js
@@ -3,6 +3,20 @@ const priceFetch = () => {
     const priceData = {};
   
     const fetchPrice = (symbol) => {
+        const userAgents = [
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+          ];
+        
+          const randomUserAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
+          const options = {
+            headers: {
+              'User-Agent': randomUserAgent,
+            },
+          };
+          
       return fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${symbol}USDT`)
         .then(response => response.json())
         .then(data => {
